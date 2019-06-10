65f08680a74b51d14a05500598ac32a7a4033ddf
pitos007 <pitos007@users.noreply.github.com>
2019-06-10 17:52:15 +0100

    delegation revisit

--- src/main/kotlin/Core/DelegatedProperties/DelegationB.kt
+++ src/main/kotlin/Core/DelegatedProperties/DelegationB.kt
@@ -12,20 +12,32 @@ class AirplaneImpl(private val where: String): Vehicle {
     override fun go() = "is flying to $where"
 }
 
-class CarOrAirplane(
-        private val model: String,
-        impl: Vehicle
-): Vehicle by impl {
+
+class CarOrAirplaneClassicDelegation(val model: String, val impl: Vehicle): Vehicle {
+
+    override fun go(): String {
+        return impl.go()
+    }
+
     fun tellMeYourTrip() {
         println("$model ${go()}")
     }
 }
 
+class CarOrAirplane(private val model: String, impl: Vehicle): Vehicle by impl {
+    private var _impl: Vehicle? = null
+
+    fun tellMeYourTrip() {
+        println("$model ${go()}")
+    }
+}
+
+
+
+
 fun main(args: Array<String>) {
-    val myAirbus330
-            = CarOrAirplane("Lamborghini", CarImpl("Seoul"))
-    val myBoeing337
-            = CarOrAirplane("Boeing 337", AirplaneImpl("Seoul"))
+    val myAirbus330 = CarOrAirplane("Lamborghini", CarImpl("Seoul"))
+    val myBoeing337 = CarOrAirplane("Boeing 337", AirplaneImpl("Seoul"))
 
     myAirbus330.tellMeYourTrip()
     myBoeing337.tellMeYourTrip()
