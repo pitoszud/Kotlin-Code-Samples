010f8a4abf2d05e4283a9bf0763dbbef61b67e3f
Pitos <pitos007@gmail.com>
2019-06-26 23:37:21 +0100

    example coroutines update

--- src/main/kotlin/Coroutines/ExampleA.kt
+++ src/main/kotlin/Coroutines/ExampleA.kt
@@ -2,11 +2,13 @@ package Coroutines
 
 import kotlinx.coroutines.*
 
+
 fun main() {
     //blockingMainA()
     //blockingBackgroundA()
     //blockingGlobal()
     blockingLocal()
+    blockingLocalDispatch()
 }
 
 
@@ -28,33 +30,49 @@ fun blockingMainB(){
 
 fun blockingBackgroundA(){
     runBlocking(Dispatchers.Default) {
-        println("one - ${Thread.currentThread().name}")
-        printDelayed("two - ${Thread.currentThread().name}")
+        println("one - ${Thread.currentThread().name}") // 1. one - DefaultDispatcher-worker-1
+        printDelayed("two - ${Thread.currentThread().name}") // 2. two - DefaultDispatcher-worker-1
     }
-    println("three - ${Thread.currentThread().name}")
+    println("three - ${Thread.currentThread().name}") // 3. three - main
+
 }
 
 
 fun blockingGlobal() = runBlocking{
-    println("one - ${Thread.currentThread().name}")
+    println("one - ${Thread.currentThread().name}") // 1. one - main
 
     val job = GlobalScope.launch {
-        printDelayed("two - ${Thread.currentThread().name}")
+        printDelayed("two - ${Thread.currentThread().name}") // 3. two - DefaultDispatcher-worker-1
     }
-    println("three - ${Thread.currentThread().name}")
+
+    println("three - ${Thread.currentThread().name}") // 2. three - main
     //delay(3000)
-    job.join() // the program can finish only after job has finished.
+    job.join() // the runBlocking can finish only after job has finished.
 }
 
 
 fun blockingLocal() = runBlocking{
-    println("one - ${Thread.currentThread().name}")
+    println("one - ${Thread.currentThread().name}") // 1. one - main
 
     launch {
-        printDelayed("two - ${Thread.currentThread().name}")
+        printDelayed("two - ${Thread.currentThread().name}") // 3. two - main
     }
-    println("three - ${Thread.currentThread().name}")
+
+    println("three - ${Thread.currentThread().name}") // 2. three - main
 }
+
+
+fun blockingLocalDispatch() = runBlocking{
+    println("one - ${Thread.currentThread().name}") // 1. one - main
+
+    launch(Dispatchers.IO) {
+        printDelayed("two - ${Thread.currentThread().name}") // 3. two - main
+    }
+
+    println("three - ${Thread.currentThread().name}") // 2. three - DefaultDispatcher-worker-1
+}
+
+
 
 
 
