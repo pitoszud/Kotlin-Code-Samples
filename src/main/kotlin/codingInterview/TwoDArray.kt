539c94eaa5b8c9a9cdb391b1b8c9b8d87862da32
Pitos <13464778+pitoszud@users.noreply.github.com>
2019-09-10 18:05:36 +0100

    init next challange

---
+++ src/main/kotlin/codingInterview/TwoDArray.kt
@@ -0,0 +1,40 @@
+package codingInterview
+
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+// Complete the hourglassSum function below.
+fun hourglassSum(arr: Array<Array<Int>>): Int {
+return 1
+
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val arr = Array<Array<Int>>(6, { Array<Int>(6, { 0 }) })
+
+    for (i in 0 until 6) {
+        arr[i] = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+    }
+
+    val result = hourglassSum(arr)
+
+    println(result)
+}
