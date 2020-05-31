1bcb5dfe23016140a05d2c6c50c15551f6f32bc1
Patryk Jakubik <patryk.jakubik@gardiff.com>
2020-05-31 09:50:50 +0100

    init glass

--- src/main/kotlin/codingInterview/TwoDArray.kt
+++ src/main/kotlin/codingInterview/TwoDArray.kt
@@ -1,23 +1,7 @@
 package codingInterview
 
-import java.io.*
-import java.math.*
-import java.security.*
-import java.text.*
-import java.util.*
-import java.util.concurrent.*
-import java.util.function.*
-import java.util.regex.*
-import java.util.stream.*
 import kotlin.collections.*
-import kotlin.comparisons.*
 import kotlin.io.*
-import kotlin.jvm.*
-import kotlin.jvm.functions.*
-import kotlin.jvm.internal.*
-import kotlin.ranges.*
-import kotlin.sequences.*
-import kotlin.text.*
 
 // Complete the hourglassSum function below.
 fun hourglassSum(arr: Array<Array<Int>>): Int {
@@ -26,15 +10,53 @@ return 1
 }
 
 fun main(args: Array<String>) {
-    val scan = Scanner(System.`in`)
 
-    val arr = Array<Array<Int>>(6, { Array<Int>(6, { 0 }) })
+    train()
 
-    for (i in 0 until 6) {
-        arr[i] = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+//    val scan = Scanner(System.`in`)
+//
+//    val arr = Array<Array<Int>>(6) { Array<Int>(6) { 0 } }
+//
+//    for (i in 0 until 6) {
+//        arr[i] = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+//    }
+
+
+
+    // val result = hourglassSum(arr)
+
+    // println(result)
+}
+
+
+fun train(){
+    val arr1 = arrayOf(1, 1, 1, 0, 0, 0)
+    val arr2 = arrayOf(0, 1, 0, 0, 0, 0)
+    val arr3 = arrayOf(1, 1, 1, 0, 0, 0)
+    val arr4 = arrayOf(0, 0, 2, 4, 4, 0)
+    val arr5 = arrayOf(0, 0, 0, 2, 0, 0)
+    val arr6 = arrayOf(0, 0, 1, 2, 4, 0)
+
+    val arr16: Array<Array<Int>> = arrayOf(arr1, arr2, arr3, arr4, arr5, arr6)
+
+    val top: Array<Int> = arr16[0].sliceArray(0..2)
+    val middle: Int = arr16[1][1]
+    val bottom: Array<Int> = arr16[2].sliceArray(0..2)
+
+    println("top")
+    top.forEach {
+        println(it.toString())
     }
 
-    val result = hourglassSum(arr)
+    println("middle")
+    println(middle)
 
-    println(result)
+    println("bottom")
+    bottom.forEach {
+        println(it)
+    }
+
 }
+
+
+
