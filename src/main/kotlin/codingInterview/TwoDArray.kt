ecd6cd991bb4bb413cd8b6e34f9e0986566ba4e0
Patryk Jakubik <patryk.jakubik@gardiff.com>
2020-06-01 17:11:46 +0100

    Adding TwoDimentionArray Solution

--- src/main/kotlin/codingInterview/TwoDArray.kt
+++ src/main/kotlin/codingInterview/TwoDArray.kt
@@ -5,31 +5,18 @@ import kotlin.io.*
 
 // Complete the hourglassSum function below.
 fun hourglassSum(arr: Array<Array<Int>>): Int {
-return 1
+    return 1
 
 }
 
 fun main(args: Array<String>) {
 
-    train()
+    sandpit()
 
-//    val scan = Scanner(System.`in`)
-//
-//    val arr = Array<Array<Int>>(6) { Array<Int>(6) { 0 } }
-//
-//    for (i in 0 until 6) {
-//        arr[i] = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
-//    }
-
-
-
-    // val result = hourglassSum(arr)
-
-    // println(result)
 }
 
 
-fun train(){
+fun sandpit() {
     val arr1 = arrayOf(1, 1, 1, 0, 0, 0)
     val arr2 = arrayOf(0, 1, 0, 0, 0, 0)
     val arr3 = arrayOf(1, 1, 1, 0, 0, 0)
@@ -39,23 +26,52 @@ fun train(){
 
     val arr16: Array<Array<Int>> = arrayOf(arr1, arr2, arr3, arr4, arr5, arr6)
 
-    val top: Array<Int> = arr16[0].sliceArray(0..2)
-    val middle: Int = arr16[1][1]
-    val bottom: Array<Int> = arr16[2].sliceArray(0..2)
 
-    println("top")
-    top.forEach {
-        println(it.toString())
+    val arrs1 = arrayOf(-9, -9, -9, 1, 1, 1)
+    val arrs2 = arrayOf(0, -9, 0, 4, 3, 2)
+    val arrs3 = arrayOf(-9, -9, -9, 1, 2, 3)
+    val arrs4 = arrayOf(0, 0, 8, 6, 6, 0)
+    val arrs5 = arrayOf(0, 0, 0, -2, 0, 0)
+    val arrs6 = arrayOf(0, 0, 1, 2, 4, 0)
+
+    val arrs16: Array<Array<Int>> = arrayOf(arrs1, arrs2, arrs3, arrs4, arrs5, arrs6)
+
+    val arrw1 = arrayOf(-1, -1, 0, -9, -2, -2)
+    val arrw2 = arrayOf(-2, -1, -6, -8, -2, -5)
+    val arrw3 = arrayOf(-1, -1, -1, -2, -3, -4)
+    val arrw4 = arrayOf(-1, -9, -2, -4, -4, -5)
+    val arrw5 = arrayOf(-7, -3, -3, -2, -9, -9)
+    val arrw6 = arrayOf(-1, -3, -1, -2, -4, -5)
+
+    val arrw16: Array<Array<Int>> = arrayOf(arrw1, arrw2, arrw3, arrw4, arrw5, arrw6)
+
+    println(calcMaxGlass(arrw16))
+
+}
+
+
+fun calcMaxGlass(arr16: Array<Array<Int>>): Int {
+
+    var maxSize = -63
+
+    for (s in 0..3) {
+        for (d in 0..3) {
+            val top = arr16[d].sliceArray(s..s + 2).sum()
+            val middle = arr16[d + 1][s + 1]
+            val bottom = arr16[d + 2].sliceArray(s..s + 2).sum()
+
+            val total = top + middle + bottom
+
+            // println(top + middle + bottom)
+
+            if (maxSize < total) {
+                maxSize = total
+            }
+        }
     }
 
-    println("middle")
-    println(middle)
 
-    println("bottom")
-    bottom.forEach {
-        println(it)
-    }
-
+    return maxSize
 }
 
 
