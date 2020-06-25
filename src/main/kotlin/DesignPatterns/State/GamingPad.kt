acf67c6ad77577265b930a5828aa0b6cca2d60ac
Patryk Jakubik <patryk.jakubik@gardiff.com>
2020-06-25 08:01:37 +0100

    state pattern cd

--- src/main/kotlin/DesignPatterns/State/GamingPad.kt
+++ src/main/kotlin/DesignPatterns/State/GamingPad.kt
@@ -1,10 +1,87 @@
 package DesignPatterns.State
 
-interface PadSetupState{
+/*
+* Defines methods for handling changes to the state
+* plus methods that can provide user instructions and return
+* the actual selected value.
+* */
+interface PadSetupState {
     fun previousPosition()
     fun nextPosition()
     fun selectPosition()
 
     fun getInstructions(): String
     fun getSelectedPosition(): Int
+}
+
+/*
+* Holds references to each state and forwards
+* to whichever is the current state.
+* */
+class Context {
+    private val verticalState: PadSetupState = Vertical(this)
+    private val horizontal: PadSetupState = Horizontal(this)
+
+
+    lateinit var currentState: PadSetupState
+
+    init {
+        // initial state will be vertical
+        setState(verticalState)
+        println(currentState.getInstructions())
+    }
+
+    fun setState(state: PadSetupState){
+        this.currentState = state
+    }
+}
+
+
+class Vertical(val context: Context) : PadSetupState {
+    var verticalPosition = 0
+    override fun previousPosition() {
+        TODO("Not yet implemented")
+    }
+
+    override fun nextPosition() {
+        TODO("Not yet implemented")
+    }
+
+    override fun selectPosition() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getInstructions(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun getSelectedPosition(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+}
+
+class Horizontal(val context: Context) : PadSetupState {
+    override fun previousPosition() {
+        TODO("Not yet implemented")
+    }
+
+    override fun nextPosition() {
+        TODO("Not yet implemented")
+    }
+
+    override fun selectPosition() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getInstructions(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun getSelectedPosition(): Int {
+        TODO("Not yet implemented")
+    }
+
+
 }
---
+++ src/main/kotlin/codingInterview/MinimumSwapsTwo.kt
@@ -0,0 +1,27 @@
+package codingInterview
+
+fun main() {
+    val nums = arrayListOf(2, 3, 4, 1, 5)
+    minSwaps(nums)
+}
+
+
+fun minSwaps(nums: ArrayList<Int>): Int{
+    val counter = 0
+    var length = nums.size
+    val arrDict = mutableMapOf<Int, Int>()
+
+    for (i in 0 until nums.size){
+        arrDict[nums[i]] = i
+        println("add to dict ${nums[i]} : $i")
+    }
+
+    val checked = arrayListOf(false)
+    nums.forEach { _ ->
+        checked.add(false)
+    }
+
+    // TODO - complete the algorithm
+
+    return counter
+}
