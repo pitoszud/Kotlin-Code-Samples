ad4bb4fa196127b86a40730c9911cbcabb029487
Patryk <pitos007@gmail.com>
2021-03-09 18:11:18 +0000

    Updating generics section

--- src/main/kotlin/Generics/MobilePhoneGen1.kt
+++ src/main/kotlin/Generics/MobilePhoneGen1.kt
@@ -1,7 +1,10 @@
 package Generics
 
+class MobilePhoneGen0(val component: Component, val componentCount: Int)
 class MobilePhoneGen1<T>(val component: T, val componentCount: Int)
 class MobilePhoneGen2<T: Component>(val component: T, val componentCount: Int)
+class MobilePhoneGen3<out T: Component>(private val component: T?, val componentCount: Int)
+class MobilePhoneGen4<in T: Component>(private val component: T?, val componentCount: Int)
 
 
 interface Component
@@ -19,6 +22,7 @@ data class Ram(
 ): Component
 
 
+
 fun main() {
     val mobileCpuGen1: MobilePhoneGen1<Cpu> = MobilePhoneGen1(Cpu(), 4)
     val ramCpuGen1: MobilePhoneGen1<Ram> = MobilePhoneGen1(Ram(), 1)
@@ -28,12 +32,26 @@ fun main() {
 
 
     // Solution - Create generic constrain <T: Component> type argument RansomwareVirus
+    // You can only use a type that extends the Component interface
     val mobileCpuGen2: MobilePhoneGen2<Cpu> = MobilePhoneGen2(Cpu(), 4)
     val ramCpuGen2: MobilePhoneGen2<Ram> = MobilePhoneGen2(Ram(), 1)
     //val infectedCpuGen2: MobilePhoneGen2<RansomwareVirus> = MobilePhoneGen2(RansomwareVirus(), 1000000000) // DOES NOT COMPILE
 
 
-    // next: Generics and Variance
+    // ---------------------------------------------------------------------------------------------------------------
+
+
+    // using out T: Component it is possible to make MobilePhoneGen4<Cpu> a subtype of MobilePhoneGen4<Component>
+    val mobileCpuGen3Cpu: MobilePhoneGen3<Cpu> = MobilePhoneGen3(Cpu(), 1)
+    val mobileCpuGen3Ram: MobilePhoneGen3<Component> = mobileCpuGen3Cpu
+
+
+    // with in T: Component it is possible to make MobilePhoneGen4<Component> a subtype of MobilePhoneGen4<Cpu>
+    val mobileCpuGen4Ram: MobilePhoneGen4<Component> = MobilePhoneGen4(Cpu(), 1)
+    val mobileCpuGen4Cpu: MobilePhoneGen4<Cpu> = mobileCpuGen4Ram
+
+
+    // next: Type Projection
     // https://www.raywenderlich.com/3634394-kotlin-generics-tutorial-getting-started
 }
 
